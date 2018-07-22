903daaa8d6df8106fdb1307e2652353f6af9ca77
Mikhail <kvilios@yandex.ru>
2018-07-23 00:25:33 +0300

    axios added

--- src/js/components/pages/home.vue
+++ src/js/components/pages/home.vue
@@ -2,12 +2,20 @@
     <div class="home">
         <!-- <img src="~@/i/components/pages/home/icon.png" alt=""> -->
         <svgicon name="components/pages/home/icon" width="200" height="200" :original="true"></svgicon>
+        {{ testData }}
     </div>
 </template>
 
 <script>
+    import axios from 'axios'
+
     export default {
         name: 'v-home',
+        data() {
+            return {
+                testData: 'Loading...'
+            }
+        },
         computed: {
             home() {
                 return this.$store.getters.pages.home
@@ -15,6 +23,9 @@
         },
         mounted() {
             document.title = this.home.title
+            axios
+                .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+                .then(response => (this.testData = response))
         }
     }
 </script>
