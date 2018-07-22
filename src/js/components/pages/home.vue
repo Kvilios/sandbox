fc95a8d7e3b76b377e182fde8fead16e68139cd4
Mikhail <kvilios@yandex.ru>
2018-07-23 00:41:57 +0300

    init

--- src/js/components/pages/home.vue
+++ src/js/components/pages/home.vue
@@ -1,21 +1,12 @@
 <template>
     <div class="home">
-        <!-- <img src="~@/i/components/pages/home/icon.png" alt=""> -->
-        <svgicon name="components/pages/home/icon" width="200" height="200" :original="true"></svgicon>
-        {{ testData }}
+        <b-alert variant="success" show>Success Alert</b-alert>
     </div>
 </template>
 
 <script>
-    import axios from 'axios'
-
     export default {
         name: 'v-home',
-        data() {
-            return {
-                testData: 'Loading...'
-            }
-        },
         computed: {
             home() {
                 return this.$store.getters.pages.home
@@ -23,9 +14,6 @@
         },
         mounted() {
             document.title = this.home.title
-            axios
-                .get('https://api.coindesk.com/v1/bpi/currentprice.json')
-                .then(response => (this.testData = response))
         }
     }
 </script>
--- src/main.js
+++ src/main.js
@@ -5,8 +5,12 @@ import router from './router'
 import 'normalize.css'
 import SvgIcon from 'vue-svgicon'
 import './svg'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.use(SvgIcon)
+Vue.use(BootstrapVue)
 
 new Vue({
   el: '#app',
